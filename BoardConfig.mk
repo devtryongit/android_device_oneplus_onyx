6eada4d693c396c82c150fe156cd4cd6770903dc
Lorenzo Bracco <devtry@riseup.net>
2017-08-10 18:51:55 +0200

    Remove snapdragon LLVM leftover
    Also remove TARGET_NEEDS_GCC_LIBC and add USE_CLANG_PLATFORM_BUILD
    
    Signed-off-by: Lorenzo Bracco <devtry@riseup.net>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,12 +52,9 @@ TARGET_KERNEL_SOURCE := kernel/oneplus/onyx
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 
-# Use Snapdragon LLVM Compiler
-TARGET_USE_SDCLANG := true
-
 # Fixes Wifi-Mobile Data toggle issue
 MALLOC_SVELTE := true
-TARGET_NEEDS_GCC_LIBC := true
+USE_CLANG_PLATFORM_BUILD := true
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
