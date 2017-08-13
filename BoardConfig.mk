adb7d4ba80efe464993894a8f8dd0041acaac2bc
Lorenzo Bracco <devtry@riseup.net>
2017-08-13 12:59:33 +0200

    Revert "onyx: Enable SVELTE to fix Wifi-Mobile Data issue"
    This reverts commit b58ef56fd44a853832670a86e71651c8d9a7e37b.
    
    Signed-off-by: Lorenzo Bracco <devtry@riseup.net>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,8 +52,6 @@ TARGET_KERNEL_SOURCE := kernel/oneplus/onyx
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 
-# Fixes Wifi-Mobile Data toggle issue
-MALLOC_SVELTE := true
 USE_CLANG_PLATFORM_BUILD := true
 
 # Webview defs
